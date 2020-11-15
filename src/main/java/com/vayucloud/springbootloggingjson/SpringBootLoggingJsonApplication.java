cd8cfa82ec005bfa7c3261a89223555833f91250
Nont Banditwong <nontster@gmail.com>
2020-11-15 23:57:21 +0700

    first commit

---
+++ src/main/java/com/vayucloud/springbootloggingjson/SpringBootLoggingJsonApplication.java
@@ -0,0 +1,13 @@
+package com.vayucloud.springbootloggingjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggingJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoggingJsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vayucloud/springbootloggingjson/SpringBootLoggingJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vayucloud.springbootloggingjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
